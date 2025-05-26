2f7facb5cd589c2b254ab0cba5be5b57d63ead44
Arailes <149024515+Arailes@users.noreply.github.com>
2025-05-26 17:37:30 +0000

    mewnow

--- client/config/env.js
+++ client/config/env.js
@@ -1,5 +1,5 @@
 module.exports = {
-    API_URL: 'https://aratech-app.onrender.com',
+    API_URL: 'https://localhost:3001',
     APP_PATH: '/usr/src/app',
     CONFIG_PATH: '/usr/src/app/config'
 }
